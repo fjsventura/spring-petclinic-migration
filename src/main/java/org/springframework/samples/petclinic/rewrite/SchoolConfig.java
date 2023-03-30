88924437a20b17f557cc35f3baaf01e806021143
Tim te Beek <tim@moderne.io>
2023-03-30 19:33:38 +0200

    Demonstrate JUnit Vintage runner on Spring Boot 2.7

--- src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
+++ src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
@@ -1,38 +1,54 @@
 package org.springframework.samples.petclinic.rewrite;
 
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SchoolConfig {
-        /** Purposely misaligned to cause auto-formatting.
-         * {@link}
-         * {@link DogSchool
+    /** Purposely misaligned to cause auto-formatting.
+     * {@link}
+     * {@link DogSchool
          * {@link DogSchool()}
          * {@link
          *      DogSchool}
-         */
+     */
     @Bean
     public DogSchool dogSchool() {
         return new DogSchool();
     }
 
-        /**
-         * <a href="
+    /**
+     * <a href="
          * https://...html">
-         * label</a>.
-         */
+     * label</a>.
+     */
     @Bean
     public CatSchool catSchool() {
         return new CatSchool();
     }
 
-        /**
-         */
+    /**
+     */
     @Bean
-    @ConditionalOnBean({CatSchool.class, DogSchool.class})
+    @Conditional(ConditionCatSchoolOrDogSchool.class)
     public VetSchool vetSchool() {
         return new VetSchool();
+    }
+
+    private static class ConditionCatSchoolOrDogSchool extends AnyNestedCondition {
+        ConditionCatSchoolOrDogSchool() {
+            super(ConfigurationPhase.REGISTER_BEAN);
+        }
+
+        @ConditionalOnBean(CatSchool.class)
+        class CatSchoolCondition {
+        }
+
+        @ConditionalOnBean(DogSchool.class)
+        class DogSchoolCondition {
+        }
     }
 }
