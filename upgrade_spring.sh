bba9e08ed5a137cedf75adb6ed9b231d66cd421c
fjventura <fernando.ventura@criticalsoftware.com>
2024-09-11 09:13:25 +0100

    Add clean

--- upgrade_spring.sh
+++ upgrade_spring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DEMO 1 - SpringBoot 3.x Best Practices
 
-mvnd -U org.openrewrite.maven:rewrite-maven-plugin:run \
+mvnd -U clean org.openrewrite.maven:rewrite-maven-plugin:run \
     -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:RELEASE \
     -Drewrite.activeRecipes=org.openrewrite.java.spring.boot3.SpringBoot3BestPractices \
     -Drewrite.exportDatatables=true
